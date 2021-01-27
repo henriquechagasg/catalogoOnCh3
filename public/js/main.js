cbe2a9b61212d04d25f7e2e49337d7d4fb33b5ec
henriquechagasg <henriquechagasg@gmail.com>
2021-01-27 20:45:24 -0300

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,9 @@
 const productCard = document.querySelectorAll('#large-card')
 const detailsButton = document.querySelectorAll("#details-btn")
 const referProductCard = document.querySelectorAll("#product-card")
+const myLazyLoad = new LazyLoad({
+    elements_selector: ".photo"
+})
 
 productCard.forEach((product) => {
     if (product.firstElementChild.firstElementChild.innerText == 'None') {
